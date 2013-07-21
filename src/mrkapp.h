c390fe1824281d71d6db81545008682004213882
Markiyan Kushnir [at home] <markiyan.kushnir@gmail.com>
2013-07-22 00:01:38 +0300

    initial commit

---
+++ src/mrkapp.h
@@ -0,0 +1,13 @@
+#ifndef MRKAPP_H_DEFINED
+#define MRKAPP_H_DEFINED
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* MRKAPP_H_DEFINED */
---
+++ src/mrkapp_private.h
@@ -0,0 +1,13 @@
+#ifndef MRKAPP_PRIVATE_H_DEFINED
+#define MRKAPP_PRIVATE_H_DEFINED
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* MRKAPP_PRIVATE_H_DEFINED */
