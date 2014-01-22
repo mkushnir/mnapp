706311e4677f750534fdc953c038c09feda1da22
Markiyan Kushnir [at home] <markiyan.kushnir@gmail.com>
2014-01-22 23:42:42 +0200

    fasthash added

--- src/mrkapp.h
+++ src/mrkapp.h
@@ -5,11 +5,14 @@
 extern "C" {
 #endif
 
+#include <stdint.h>
+
 const char *mrkapp_diag_str(int);
 
 int local_server(int, void **);
 void local_server_shutdown(void);
 void daemonize(const char *, const char *, const char *);
+uint64_t fasthash(uint64_t, const unsigned char *, size_t);
 
 #ifdef __cplusplus
 }
